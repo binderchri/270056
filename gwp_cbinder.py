f8e7ee351f3296d9418e56528f6a04a4db7d9a2f
binderchri <binderchri@gmail.com>
2016-04-17 23:01:01 +0200

    gwp finished

--- gwp_cbinder.py
+++ gwp_cbinder.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+import argparse
+from itertools import groupby
 
 
 class Node:
@@ -7,18 +8,14 @@ class Node:
         self.value = value
         self.minChild = None
 
-        isEndnode = value == 0
-        self.minDepth = 0 if isEndnode else None
-        self.calculated = isEndnode
+        self.isEndnode = value == 0
+        self.minDepth = 0 if self.isEndnode else None
+        self.calculated = self.isEndnode
 
         # valid when there's a mint smaller than the amount
-        self.valid = value >= min(controller.mints) or isEndnode
-
-        # print("Node created with value", value)
+        self.valid = value >= min(controller.mints) or self.isEndnode
 
     def calculateMinDepth(self):
-        # print("calculateMinDepth for Node with value", self.value)
-
         allNodes = (self.controller.getNode(self.value - mint) for mint in self.controller.getMints() if mint <= self.value)
         validNodesWithDepth = [nwd for nwd in [(node, node.getMinDepth()) for node in allNodes if node.valid] if nwd[0].valid]
 
@@ -28,9 +25,8 @@ class Node:
             return None
 
         nodeWithMinDepth = min(validNodesWithDepth, key=lambda nwd: nwd[1])
-        self.minDepth = nodeWithMinDepth[1]
+        self.minDepth = nodeWithMinDepth[1] + 1
         self.minChild = nodeWithMinDepth[0]
-
 
         self.calculated = True
         return self.minDepth
@@ -40,29 +36,26 @@ class Node:
 
 class Controller:
     def __init__(self, mints):
-        self.mints = mints
+        self.mints = sorted([int(mint * 1000) for mint in mints], reverse=True)
         self.nodes = dict()
 
     def run(self, amount):
-        endNode = self.getNode(0)
+        self.getNode(0) # create end-node
+        node = self.getNode(int(amount * 1000))
+        depth = node.getMinDepth() # do the calculation
 
-        node = self.getNode(amount)
-        depth = node.getMinDepth()
-
-        print("========")
-
-        print("Min Depth:", depth)
-
-        for validNode in sorted((node for node in self.nodes.values() if node.valid), key=lambda node: node.value, reverse=True):
-            print("Node with value", validNode.value)
-
-        print("========== Iteration:")
-        while node is not None:
-            print("Node", node.value)
+        valueslist = list()
+        while node is not None and node:
+            valueslist.append(node.value)
             node = node.minChild
 
+        changeCoins = sorted([orig-shifted for orig, shifted in zip(valueslist[:-1], valueslist[1:])])
 
+        assert depth == len(changeCoins)
 
+        for coin, group in groupby(changeCoins):
+            print("%i x %.2f coin" % (len(list(group)), coin / 1000))
+        print("Total coins needed: ", depth)
 
     def __createNode(self, value):
         self.nodes[value] = Node(value, self)
@@ -75,5 +68,11 @@ class Controller:
         return self.mints
 
 
-c = Controller([3,5])
-c.run(11)
+parser = argparse.ArgumentParser(description='Change coin problem')
+parser.add_argument("-a", required=True, dest="amount", type=float, help="The amount of change")
+parser.add_argument('-c', dest="coins", type=float, nargs='+', default=[0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2],
+                    help="The coins")
+args = parser.parse_args()
+
+c = Controller(args.coins)
+c.run(args.amount)
--- mtp_cbinder.py
+++ mtp_cbinder.py
@@ -2,17 +2,20 @@ costs = dict()
 matrix = dict()
 
 # -1 ... not possible
+
+#costs[(i,j)] = (L,D) # i is horizontal, j is vertical
+
 costs[(0,0)] = (3,1)
 costs[(1,0)] = (3,0)
-costs[(2,0)] = (-1,2)
+costs[(2,0)] = (None,2)
 
 costs[(0,1)] = (3,4)
 costs[(1,1)] = (2,6)
-costs[(2,1)] = (-1,5)
+costs[(2,1)] = (None,5)
 
-costs[(0,2)] = (0,-1)
-costs[(1,2)] = (7,-1)
-costs[(2,2)] = (-1,-1)
+costs[(0,2)] = (0,None)
+costs[(1,2)] = (7,None)
+costs[(2,2)] = (None,None)
 
 def R(i,j):
     return costs[(i,j)][0]
@@ -26,13 +29,23 @@ def C(i,j):
 
     return 0
 
+width = 3
+height = 3
 
-for i in range(1,3):
-    for j in range(1,3):
-        cr = C(i-1, j) + R(i-1, j)
-        cd = C(i, j-1) + D(i, j-1)
+for i in range(0,width):
+    l = "+"
+    for j in range(0,height):
+        l+="--" + str(costs[(i,j)][0]) + "--+" #invert i and j!!!
+    print(l)
 
-#        print (i,j,cr,cd)
+
+
+for i in range(0,width):
+    for j in range(0,height):
+        cr = C(i-1, j) + R(i-1, j) if i > 0 else 0
+        cd = C(i, j-1) + D(i, j-1) if j > 0 else 0
+
+        print("i", i, "j", j, "cr", cr, "cd", cd)
 
         matrix[(i,j)] = max(cr, cd)
 
